1952f3ba376cbc6c672209d146db74287ce5681d
radoslavbunic-dev <radoslav.bunic@gmail.com>
2026-04-20 23:30:02 +0200

    added skin prefabs and boards with 9 positions in each board

---
+++ Assets/Scripts/GameLogic/Board.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+	[SerializeField] Transform[] cells;
+}
--- Assets/Scripts/Templates/SkinTemplate.cs
+++ Assets/Scripts/Templates/SkinTemplate.cs
@@ -9,13 +9,16 @@ public class SkinTemplate : Template
     [SerializeField] GameObject boardPrefab;
     [SerializeField] Sprite boardSprite;
     [SerializeField] Sprite xSprite;
+    [SerializeField] GameObject xPrefab;
     [SerializeField] Sprite oSprite;
+    [SerializeField] GameObject oPrefab;
     [SerializeField] int order;
-
 
     public GameObject BoardPrefab { get { return boardPrefab; } }
     public Sprite BoardSprite { get { return boardSprite; } }
     public Sprite XSprite { get { return xSprite; } }
+    public GameObject XPrefab { get { return xPrefab; } }
     public Sprite OSprite { get { return oSprite; } }
+    public GameObject OPrefab { get { return oPrefab; } }
     public int Order { get { return order; } }
 }
